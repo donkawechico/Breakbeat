41cf8f15bde8947d7a0f326c73be8addf34ba20a
Reece Selby <reece.selby@gmail.com>
2014-02-10 19:39:13 +0000

    Extreme alpha (Breakbeat works)
    Get the module to work + export a rough version of what the final result
    should be

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pydub
+import random
 
+drums = pydub.AudioSegment.from_wav("amen.wav")
+#half = drums[:len(drums)/2]
+#half.export("test.wav", format="wav")
+
+fast = pydub.effects.speedup(drums, chunk_size=50)
+chopped = []
+slice_length = len(fast) /16
+for n in range(1,16):
+    chopped.append(fast[(n-1) * slice_length:n * slice_length])
+random.shuffle(chopped)
+final_drums = chopped[random.randint(0,14)] + chopped[random.randint(0,14)] + chopped[random.randint(0,14)] + chopped[random.randint(0,14)] + chopped[random.randint(0,14)] + chopped[random.randint(0,14)] + chopped[random.randint(0,14)] + chopped[random.randint(0,14)] + chopped[random.randint(0,14)]
+final_drums.export("test_random.wav", format="wav")
